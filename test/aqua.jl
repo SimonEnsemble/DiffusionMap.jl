9289d0a5b201faaab312fa044fc0c81cc423912b
eahenle <eahenle@gmail.com>
2023-02-26 17:30:30 -0800

    workflows

---
+++ test/aqua.jl
@@ -0,0 +1,15 @@
+module Test_aqua
+
+using DiffusionMap
+import Aqua.test_all
+
+# ambiguity testing finds many "problems" outside the scope of this package
+ambiguities = false
+
+# to skip when checking for stale dependencies and missing compat entries
+#   Aqua is added in a separate CI job, so (ironically) does not work w/ itself
+stale_deps = (ignore=[:Aqua],)
+
+test_all(DiffusionMap; ambiguities=ambiguities, stale_deps=stale_deps)
+
+end
